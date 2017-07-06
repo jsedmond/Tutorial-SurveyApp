c32508c1048ac01611c90d741fe3da39edecda3f
Jacob Edmond <jacobedmond@Jacobs-MacBook-Pro.local>
2017-07-05 23:04:13 -0500

    Set up the skeleton of the Survey App.

---
+++ SurveyApp/FirstViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FirstViewController.swift
+//  SurveyApp
+//
+//  Created by Jacob Edmond on 7/5/17.
+//  Copyright © 2017 Jacob Edmond. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SurveyApp/ViewController.swift
+++ SurveyApp/ViewController.swift
@@ -9,17 +9,79 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
+    
+    @IBOutlet weak var containerView: UIView!
+    
+    @IBOutlet weak var progressLabel: UILabel!
+
+    var currentViewController:UIViewController?
+    
+    var pageIndex = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func nextTapped(_ sender: Any) {
 
-
+        
+        switch (pageIndex) {
+        case 1:
+            // Go to Second View Controller
+            let nextViewController = storyboard?.instantiateViewController(withIdentifier: "SecondVC")
+            
+            if let fromVC = currentViewController, let toVC = nextViewController {
+                
+                switchChildViewControllers(fromVC: fromVC, toVC: toVC)
+            }
+            
+            break
+            
+        case 2:
+            // Go to Third View Controller
+            break
+            
+        default:
+            break
+        }
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        currentViewController = segue.destination
+    }
+    
+    func switchChildViewControllers(fromVC:UIViewController, toVC:UIViewController) {
+        
+        // Tell the current VC that it's transitioning
+        fromVC.willMove(toParentViewController: nil)
+        
+        // Add the new VC
+        self.addChildViewController(toVC)
+        containerView.addSubview(toVC.view)
+        
+        // Remove the old VC
+        fromVC.view.removeFromSuperview()
+        fromVC.removeFromParentViewController()
+        
+        // Size the from of the toVC
+        toVC.view.frame = containerView.bounds
+        
+        // Tell the new VC that it has transitioned
+        toVC.didMove(toParentViewController: self)
+        
+        // Set the currentViewController property to toVC
+        currentViewController = toVC
+        
+    }
+    
 }
-
